38b4663da8fff9f190f450b8e2dc13521ecad739
Aaditya <addyrockx96@gmail.com>
2019-02-24 19:50:36 +0530

    Initial COmmit

---
+++ tensor_first.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+
+dataset = tf.data.Dataset.from_tensor_slices((images, labels)).shuffle(1000).batch(32)
+
+
+for image, label in dataset:
+    print(image)
